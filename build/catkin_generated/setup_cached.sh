f45c9d9240812675f719f8450a2dc1bcf848a189
hoaiphu1002 <phuhoaitruongvu@gmail.com>
2026-04-08 07:03:48 +0700

    nhanh_datn

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/hoai_phu/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/hoai_phu/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/hoai_phu/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/hoai_phu/catkin_ws/src:$ROS_PACKAGE_PATH"
